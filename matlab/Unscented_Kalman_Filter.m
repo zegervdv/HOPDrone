8fe64a946497fbcf6e6f712aae5b6f2916e1ace4
zegervdv <zegervdv@me.com>
2014-03-05 17:25:58 +0100

    Part one of Kalman Filter

--- matlab/Unscented_Kalman_Filter.m
+++ matlab/Unscented_Kalman_Filter.m
@@ -1,29 +1,59 @@
-%sampleperiode
-Ts;
+% HOP 2014
+% Unscented Kalman Filter
 
-%snelheid initieel resetten (op 0) en dan laten accumuleren
-v_X; 
-v_Y;
-v_Z;
-%posities volgens accelerometer -- initieel resetten (op x_bias,y_bias,z_bias) en dan laten accumuleren
-Xa;
-Ya;
-Za;
-%versnelling output accelerometer
-a_X; 
-a_Y; 
-a_Z;
+% Constants
+Nx = 6;
+kappa = 2;
+alpha = 1e-3;
+beta = 0;
+Fs = 1e3;
+deltaT = 1 / Fs;
 
-%huidige snelheid op basis van vorige snelheid en gemeten versnelling
-v_X = v_X + Ts*a_X;
-v_Y = v_Y + Ts*a_Y;
-v_Z = v_Z + Ts*a_Z;
-%huidige positie op basis van vorige positie en gemeten snelheid
-Xa=Xa + v_X*Ts;
-Ya=Ya + v_Y*Ts;
-Za=Za + v_Z*Ts;
+var_u = 1 * eye(Nx/2); % Noise variance on prediction: 1m
 
-%output gyroscoop
-yaw_rate;
-pitch_rate;
-roll_rate;
+% Assume start at (0,0,0) at 0 velocity
+prevX = zeros(Nx, 1);
+X = zeros(Nx, 1);
+
+var = 5 * eye(Nx);
+
+% N = 6 => 13 sigma points
+sigma = zeros(2*Nx + 1, Nx);
+
+% Weights
+Wm = ones(1, Nx + 1);
+Wc = ones(1, Nx + 1);
+
+Wm = 1/(2*(Nx + kappa)) * Wm;
+Wc = 1/(2*(Nx + kappa)) * Wc;
+
+Wm(1) = kappa/(Nx + kappa);
+Wc(1) = kappa/(Nx + kappa) + (1 - alpha^2 + beta);
+
+% Assume function F to be linear
+F = eye(Nx);
+for i = 1:Nx/2
+    F(i, Nx/2 + i) = deltaT;
+end
+
+Gup = eye(Nx/2) * deltaT^2/2;
+Glo = eye(Nx/2) * deltaT;
+G = [Gup; Glo];
+
+
+for k = 1:100
+    % Calculate sigmapoints
+    sigma(1, :) = prevX';
+    root = sqrt(Nx + kappa) * chol(var, 'lower');
+    for i = 2:Nx+1
+       sigma(i, :) = prevX' + root(i-1, :);
+       sigma(Nx + i, :) = prevX' - root(i-1, :);
+    end
+    
+    % Prediction
+    mkmin = F * prevX;
+    Pkmin = F * var * F' + G * var_u * G';
+    
+    % Measurement update
+    Y = F * sigma'
+end
