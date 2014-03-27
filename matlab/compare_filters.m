83b03eccbda3c483673bcf06fefd756c4362e617
lgbogaer <laurens.bogaert@hotmail.com>
2014-03-27 15:04:55 +0100

    add sample frequency as parameter

--- matlab/compare_filters.m
+++ matlab/compare_filters.m
@@ -12,7 +12,7 @@ steps = 1000; % Number of timesteps
 Nx = 9;
 alpha = 1e-3;
 beta = 2;
-Fs = 5; % 1O Hz sampling
+Fs = 5; % 5 Hz sampling
 deltaT = 1 / Fs;
 kappa = alpha^2*Nx - Nx;
 std_meas = 10e-2; % Standard deviation on measurement 5cm
@@ -40,8 +40,8 @@ for k = 2:steps
 end
 
 lms = least_mean_squares(pos(1:3,:), 0.05);
-ukf = Unscented_Kalman_Filter(pos, 0.05, F, G, Nx, 0);
-ukf_acc = Unscented_Kalman_Filter(pos, 0.05, F, G, Nx,1);
+ukf = Unscented_Kalman_Filter(pos, 0.05, F, G, Nx,Fs, 0);
+ukf_acc = Unscented_Kalman_Filter(pos, 0.05, F, G, Nx, Fs, 1);
 
 figure(1);
 plot(pos(1,:), pos(2,:));
