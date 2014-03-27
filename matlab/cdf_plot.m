5391c51cd239184bd8307225292320948b3ecc81
lgbogaer <laurens.bogaert@hotmail.com>
2014-03-27 14:58:59 +0100

    more detailed cdf

--- matlab/cdf_plot.m
+++ matlab/cdf_plot.m
@@ -3,7 +3,7 @@ function [ cdf ] = cdf_plot( err, opts )
 %   Detailed explanation goes here
 steps = size(err,2);
 cdf = zeros(1, ceil(max(err))*10);
-err_point = [0:.1:ceil(max(err))];
+err_point = [0:.01:ceil(max(err))];
 
 for i = 1:length(err_point)
     cdf(i) = sum(err <= err_point(i)) / steps;
