2f713fed7a0f5f2f7c5704b4410992b27f6d1440
zegervdv <zegervdv@me.com>
2014-05-03 11:04:58 +0200

    Remove 3D code from master

--- code/drone/lms.h
+++ code/drone/lms.h
@@ -1,22 +1,5 @@
-/**
- *****************************************************************************
- **
- **  File        : lms.h
- **
- **  Abstract    : Calculates 2D and 3D position using LMS. 
- **
- **  Functions   : Calculate2DPosition, Calculate3DPosition, 
- ** 			   GetIntersections, Get3DIntersections
- **
- **  Authors     : Thomas Deckmyn (added 3D localization)
- **  
- *****************************************************************************
- */
-
 #include "arm_math.h"
 
 arm_status Calculate2DPosition(uint8_t nrAnchors, float32_t* currPosEst, float32_t* anchorsX, float32_t* anchorsY, uint32_t* d);
-arm_status Calculate3DPosition(uint8_t nrAnchors, float32_t* currPosEst, float32_t* anchorsX, float32_t* anchorsY, float32_t* anchorsZ, uint32_t* d);
 uint8_t getIntersections(uint32_t* data, float32_t* results);
-uint8_t get3DIntersections(float32_t* data, float32_t* results);
 
